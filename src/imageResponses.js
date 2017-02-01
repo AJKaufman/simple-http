675ff0bc7245ce0aef3899f58885ab372315c43a
AJKaufman <ak3012@rit.edu>
2017-02-01 13:00:37 -0500

    Added all necessary files

---
+++ src/imageResponses.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const spongegarImg = fs.readFileSync(`${__dirname}/../client/spongegar.png`);
+
+
+const dankmemes = (request, response) => {
+        
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.write(spongegarImg);
+    response.end();
+};
+
+
+module.exports.dankmemes = dankmemes;
